c13a075c431723923e49bd53c2e9557f12cdcbfb
Vaibhav_Shukla <vaibhavshukla2704@gmail.com>
2023-06-13 11:25:58 +0530

    changes some in the env related issue

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
-const BASE_URL = 'https://api.themoviedb.org/3';
+const BASE_URL = "https://api.themoviedb.org/3";
 
-const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+const TMDB_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 
 const headers = {
-  Authorization: 'bearer ' + TMDB_TOKEN,
+  Authorization: "bearer " + TMDB_TOKEN,
 };
 
 export const fetchDataFromApi = async (url, params) => {
